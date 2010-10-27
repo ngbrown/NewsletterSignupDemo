f44cfc18289b7764ba555c3c00b2c2d4337e2d6b
ngbrown <nathan@nkbrown.us>
2010-10-27 00:23:33 -0500

    Rearranged directories and added Ninject and logging.

--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -1,16 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Routing;
-
-namespace Web
+﻿namespace Web
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Web.Routing;
+    using System.Reflection;
+
+    using Ninject;
+    using Ninject.Extensions.Logging;
+    using Ninject.Modules;
+    using Ninject.Web.Mvc;
+
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : NinjectHttpApplication
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
@@ -24,11 +30,30 @@ namespace Web
 
         }
 
-        protected void Application_Start()
+        protected override IKernel CreateKernel()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+
+            var kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+            return kernel;
+        }
+
+        protected override void OnApplicationStarted()
         {
             AreaRegistration.RegisterAllAreas();
-
+            
             RegisterRoutes(RouteTable.Routes);
+
+            this.Kernel.Get<ILoggerFactory>().GetLogger(this.GetType())
+                .Info("Application Started.");
+        }
+
+        class SiteModule : NinjectModule
+        {
+            public override void Load()
+            {
+            }
         }
     }
 }
